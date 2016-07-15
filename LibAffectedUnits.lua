b58cb6fa88c5af3b3766a9c47a6bcdcbea5ffb2c
Ron Waldon <jokeyrhyme@gmail.com>
2016-07-16 07:14:31 +1000

    monitoring certain player actions...

--- LibAffectedUnits.lua
+++ LibAffectedUnits.lua
@@ -1,15 +1,37 @@
+local playerGUID
 
-local function HelloWorld()
-  print("Hello, world!");
+-- generating our set of monitored event types
+local usefulEventTypePrefixes = {
+  "SWING", "RANGE", "SPELL", "SPELL_PERIODIC", "SPELL_BUILDING"
+}
+local usefulEventTypeSuffixes = {
+  "_DAMAGE", "_HEAL", "_INTERRUPT"
+}
+local usefulEventTypes = {}
+for i, prefix in ipairs(usefulEventTypePrefixes) do
+  for i, suffix in ipairs(usefulEventTypeSuffixes) do
+    tinsert(usefulEventTypes, prefix .. suffix)
+  end
 end
 
-HelloWorld();
+-- global: referenced from XML
+function LibAffectedUnits_OnLoad ()
+  playerGUID = UnitGUID("player")
+end
 
-function LibAffectedUnits_OnEvent ()
-  print("PLAYER_ENTERING_WORLD");
+local function LibAffectedUnits_OnEvent (self, event, ...)
+  local timestamp, eventtype, hideCaster, srcGUID, srcName, srcFlags, srcRaidFlags, dstGUID, dstName, dstFlags, dstRaidFlags = select(1, ...)
+
+  -- only want our own actions, not anyone else's
+  if srcGUID ~= playerGUID then return end
+
+  -- only process useful events
+  if not tContains(usefulEventTypes, eventtype) then return end
+
+  -- print("LibAffectedUnits_OnEvent() " .. srcGUID)
 end
 
 local eventFrame = CreateFrame("Frame");
 eventFrame:SetScript("OnEvent", LibAffectedUnits_OnEvent);
 
-eventFrame:RegisterEvent("PLAYER_ENTERING_WORLD");
+eventFrame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
--- README.md
+++ README.md
@@ -6,3 +6,5 @@ re-learning WoW Lua; building a helper to count units affected by player actions
 ## Resources
 
 - http://wowwiki.wikia.com/wiki/AddOn_programming_tutorial/Introduction
+
+- http://wowwiki.wikia.com/wiki/API_COMBAT_LOG_EVENT
