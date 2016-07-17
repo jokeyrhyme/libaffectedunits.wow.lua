1aedf8ca6aebc57c815178ec8798538285f5086a
Ron Waldon <jokeyrhyme@gmail.com>
2016-07-17 14:41:23 +1000

    aggregate counts of affected units of n seconds

--- LibAffectedUnits.lua
+++ LibAffectedUnits.lua
@@ -15,6 +15,16 @@ local secondsRing = {};
 -- e.g. { { ["harm"] = { ... }; ["help"] = { ... } }, ... }
 local affectedRing = {};
 
+-- e.g.
+-- {
+--   {
+--     ["harm"] = { ["min"] = 0; ["max"] = 0; ["avg"] = 0 },
+--     ["help"] = { ["min"] = 0; ["max"] = 0; ["avg"] = 0 }
+--   },
+--   ...
+-- }
+local aggregates = {};
+
 local function wipeAffected (affected)
   -- e.g. affected = { ["harm"] = { ... }; ["help"] = { ... } }
   if affected == nil then
@@ -38,33 +48,73 @@ local function bumpRingsIfNecessary (now)
   end
 end
 
+local aggregateGUIDs = { ["harm"] = {}; ["help"] = {} };
+
+local function reduceAggregates (aggregates, index, affected)
+  local aggregate = aggregates[index] or { ["harm"] = 0; ["help"] = 0 };
+
+  if not affected then
+    aggregate["harm"] = 0;
+    aggregate["help"] = 0;
+    return aggregate;
+  end
+
+  for i, affect in ipairs({ "harm", "help" }) do
+    for j, harmGUID in ipairs(affected[affect]) do
+      if not tContains(aggregateGUIDs[affect], harmGUID) then
+        tinsert(aggregateGUIDs[affect], harmGUID);
+      end
+    end
+
+    aggregate[affect] = # aggregateGUIDs[affect];
+  end
+
+  return aggregate;
+end
+
+local function bumpAggregates ()
+  local aggregatesIndex = 1;
+  local aggregatesRingPosition = currentRingPosition;
+  local output = "";
+  wipe(aggregateGUIDs["harm"]);
+  wipe(aggregateGUIDs["help"]);
+  while aggregatesRingPosition >= MIN_POSITION do
+    output = output .. " " .. date("%S", secondsRing[aggregatesRingPosition] or 0);
+    aggregates[aggregatesIndex] = reduceAggregates(aggregates, aggregatesIndex, affectedRing[aggregatesRingPosition])
+    aggregatesIndex = aggregatesIndex + 1;
+    aggregatesRingPosition = aggregatesRingPosition - 1;
+  end
+  aggregatesRingPosition = MAX_POSITION;
+  while aggregatesRingPosition > currentRingPosition do
+    output = output .. " " .. date("%S", secondsRing[aggregatesRingPosition] or 0);
+    aggregates[aggregatesIndex] = reduceAggregates(aggregates, aggregatesIndex, affectedRing[aggregatesRingPosition])
+    aggregatesIndex = aggregatesIndex + 1;
+    aggregatesRingPosition = aggregatesRingPosition - 1;
+  end
+end
+
 -- for debugging only
-local function dumpRings ()
+local function dumpState ()
   local output = "";
   for i, timestamp in ipairs(secondsRing) do
     output = output .. " " .. date("%S", timestamp);
   end
   print("LibAffectedUnits:" .. output);
+
   output = "";
   for i, affected in ipairs(affectedRing) do
     output = output .. " " .. # affected["harm"] .. ":" .. # affected["help"];
   end
   print("LibAffectedUnits:" .. output);
+
+  output = "";
+  for i, aggregate in ipairs(aggregates) do
+    output = output .. " " .. aggregate["harm"] .. ":" .. aggregate["help"];
+  end
+  print("LibAffectedUnits:" .. output);
 end
 
--- generating our set of monitored event types
-local usefulEventTypePrefixes = {
-  "SWING", "RANGE", "SPELL", "SPELL_PERIODIC", "SPELL_BUILDING"
-}
-local usefulEventTypeSuffixes = {
-  "_DAMAGE", "_HEAL", "_INTERRUPT"
-}
-local usefulEventTypes = {}
-for i, prefix in ipairs(usefulEventTypePrefixes) do
-  for i, suffix in ipairs(usefulEventTypeSuffixes) do
-    tinsert(usefulEventTypes, prefix .. suffix)
-  end
-end
+local usefulEventTypes;
 
 local function recordAffectedGUID (dstGUID, dstFlags)
   local guids;
@@ -101,6 +151,20 @@ local eventFrame;
 
 function lib:OnInitialize()
   playerGUID = UnitGUID("player");
+
+  -- generating our set of monitored event types
+  local usefulEventTypePrefixes = {
+    "SWING", "RANGE", "SPELL", "SPELL_PERIODIC", "SPELL_BUILDING"
+  }
+  local usefulEventTypeSuffixes = {
+    "_DAMAGE", "_HEAL", "_INTERRUPT"
+  }
+  usefulEventTypes = {}
+  for i, prefix in ipairs(usefulEventTypePrefixes) do
+    for i, suffix in ipairs(usefulEventTypeSuffixes) do
+      tinsert(usefulEventTypes, prefix .. suffix)
+    end
+  end
 end
 
 function lib:OnEnable()
@@ -120,6 +184,8 @@ function lib:OnDisable()
 end
 
 function lib:TimerFeedback()
-  bumpRingsIfNecessary(time());
-  -- dumpRings();
+  local now = time();
+  bumpRingsIfNecessary(now);
+  bumpAggregates();
+  -- dumpState();
 end
